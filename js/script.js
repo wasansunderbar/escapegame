fc3efa3c70fe5bf3de1bf07aeda4b3832228174c
wasansunderbar <110877592+wasansunderbar@users.noreply.github.com>
2023-07-11 07:15:29 +0900

    4층추가

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function setSessionStorage(key, value){
+    sessionStorage.setItem(key,value); 
+}
+
+function getSessionStorage(key){ 
+    return sessionStorage.getItem(key);
+ } 
+
+ function removeSessionStorage(key){
+     sessionStorage.removeItem(key);
+ } 
